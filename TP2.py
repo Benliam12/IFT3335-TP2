6e23f6511639b7f53ddd6cd7810d4d79f20f81ea
Vanessa-T-Soucy <vanessa.soucy@hotmail.com>
2022-12-17 10:38:09 -0500

    Update TP2.py

--- TP2.py
+++ TP2.py
@@ -1,8 +1,14 @@
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
 
 data = [] # Would load the text data from what we should do our testing
 
-
+# Shit to do:
+# 1 - Lire data for txt
+# 2 - Information contextuels de la phrase: mots avant/ apres, catégories des mots autour, ...
+# 3 - 
 
 
 #Naive Bayes
