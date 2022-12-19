2b31869f10b2b18a7039e20876e0f01bf26d50e0
Benliam12 <s100live.ca@live.ca>
2022-12-18 22:31:22 -0500

    Patch CountVectorizer

--- TP2.py
+++ TP2.py
@@ -7,26 +7,14 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn import tree, svm
 file = open("data.txt", "r")
-
 data = []
-
-c1 = 0
-
 for l in file.readlines():
-    if c1 == 10000:
-        break
-
     if "$$" in l:
         continue
-
     content = l.split(" ")
-
-    r = []  # [sens#, word1, tword1, word2, tword2, word3, tword3, word4, tword4]
     sentence = []
-    index = -1
     c = 0
     interest = 0
-    w = ""
     for t in content:
         if "=" in t:
             continue
@@ -38,18 +26,12 @@ for l in file.readlines():
         tt = t.split("/")
         if len(tt) == 2:
             if "interest" in t and "_" in t:
-                w = t
                 interest = c
             sentence.append(tt)
             c += 1
 
-    d = ["@" for x in range(9)]
-
-    tt = sentence[interest][0].split("_")
-    if len(tt) != 2:
-        pass
-    else:
-        d[0] = tt[1]
+    d = ["@" for _ in range(9)]
+    d[0] = sentence[interest][0].split("_")[1]
 
     if (interest-1) >= 0:
         d[1] = sentence[interest-1][0]
@@ -64,7 +46,6 @@ for l in file.readlines():
         d[7] = sentence[interest+2][0]
         d[8] = sentence[interest+2][1]
     data.append(d)
-    c1 += 1
 
 t = []
 a = []
@@ -73,15 +54,18 @@ for i in data:
     a.append(i[0])
 
 
-vectorizer = CountVectorizer(
-    token_pattern=r"\b\w+\-\w+\b|\b\w+\.\w+\.\b|\b\w+\.\w+\b|\b\w+\b|\@|\,|\.|\%|\`\`|\'\'")
+def custom(text):
+    return text.split(" ")
+
+
+vectorizer = CountVectorizer(tokenizer=custom)
 X = vectorizer.fit_transform(t).toarray()
 tokenizer = vectorizer.build_tokenizer()
 output_corpus = []
 na = []
 for i, element in enumerate(t):
-    tt = element
     element = tokenizer(element.lower())
+    # On s'assure d'avoir le bon format.
     if(len(element) == 8):
         output_line = []
         for token in element:
@@ -89,8 +73,8 @@ for i, element in enumerate(t):
         output_corpus.append(output_line)
         na.append(a[i])
 
-# print(2368-len(na))
 
+# Test de performance
 X_train, X_test, y_train, y_test = train_test_split(
     output_corpus, na, test_size=0.5, random_state=0)
 gnb = MultinomialNB()
@@ -121,19 +105,3 @@ clf2 = clf2.fit(output_corpus, na)
 y_pred = clf2.predict(X_test)
 print("MLP Number of mislabeled points out of a total %d points : %d" %
       (len(X_test), (y_test != y_pred).sum()))
-
-# Shit to do:
-# 1 - Lire data for txt
-# 2 - Information contextuels de la phrase: mots avant/ apres, catégories des mots autour, ...
-# 3 -
-
-
-# Naive Bayes
-
-# Decision tree
-
-# Random Forest
-
-# SVM
-
-# Multi Layers Perceptron
